f846c2871b51a91678cdac8f41c346ae210c8144
dmunter2 <devon.munter@gmail.com>
2019-07-21 18:41:37 -0700

    header-working

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,33 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+
+
+function Header() {
+    const header = document.createElement('div');
+    const span = document.createElement('span');
+    const h1 = document.createElement('h1');
+    const span2 = document.createElement('span');
+
+
+    header.classList.add('header');
+    span.classList.add('temp')
+    span2.classList.add('date');
+
+
+    header.appendChild(span);
+    header.appendChild(h1);
+    header.appendChild(span2);
+
+
+    h1.textContent = 'Lambda Times';
+    span.textContent = "MARCH 28, 2019";
+    span2.textContent = '98';
+
+    return header;
+
+}
+
+const headerContainer = document.querySelector('.header-container');
+
+headerContainer.appendChild(Header());
